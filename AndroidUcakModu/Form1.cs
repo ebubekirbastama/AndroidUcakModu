10551f8dafb06a246933c845bbbf8f2623600f25
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-02-11 17:31:59 +0200

    Add project files.

---
+++ AndroidUcakModu/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace AndroidUcakModu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProcessStartInfo pinfo = new ProcessStartInfo();
+        Process proc = new Process();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changeIP("shell am start -a android.settings.AIRPLANE_MODE_SETTINGS");
+            changeIP("shell input keyevent 21 && input keyevent 22 && input keyevent 23");
+        }
+
+        void changeIP(string cml)
+        {
+            pinfo.FileName = Application.StartupPath + "\\adb.exe";
+            pinfo.Arguments = cml;
+            proc.StartInfo = pinfo;
+            proc.Start();
+        }
+    }
+}
